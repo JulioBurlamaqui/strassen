b50fa9622d5480245ada3b33a801d72c9e4d9f31
Júlio Burlamaqui <64053910+JulioBurlamaqui@users.noreply.github.com>
2022-09-20 22:48:09 -0300

    Debugando
    esqueci de inicializar as matrizes kkkkkkkkkkk juro vei não dá 10 de qi

--- Q1_strassen.c
+++ Q1_strassen.c
@@ -46,13 +46,28 @@ int **subtrai(int **A, int **B, int grau)
 
 void strassen(int **A, int **B, int **C, int grau)
 {
+
     int i, j, k;
+    
+	if(grau <= 2)
+	{//Se for menor ou igual a dois faça normalmente
+   		for(i = 0; i < grau; i++)
+		{
+			for(j = 0; j < grau; j++)
+			{
+				C[i][j] = 0;
+				for(k = 0; k < grau; k++)
+					C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+		
+        return;
+    }
+    
     int **A11, **A12, **A21, **A22;
     int **B11, **B12, **B21, **B22;
     int **C11, **C12, **C21, **C22;
     int **M1, **M2, **M3, **M4, **M5, **M6, **M7;
-    
- 	printf("%d  %d\n%d  %d\n\n", A[0][0], A[0][1], A[1][0], A[1][1]);
     
     {//Aloca dinamicamente
 		A11 = malloc(sizeof(int)*grau/2);
@@ -88,78 +103,51 @@ void strassen(int **A, int **B, int **C, int grau)
 		    M5[i] = malloc(sizeof(int)*grau/2);
 		    M6[i] = malloc(sizeof(int)*grau/2);
 		    M7[i] = malloc(sizeof(int)*grau/2);	
-		}	
-	}
-    
-    if(grau <= 2)
-	{
-   		for(i = 0; i < grau; i++)
-		{
-			for(j = 0; j < grau; j++)
-			{
-				C[i][j] = 0;
-				for(k = 0; k < grau; k++)
-					C[i][j] += A[i][k] * B[k][j];
-			}
 		}
-	
-		printf("FOI");
 		
-		{//Libera espaços	
- 	    	for(i = 0; i < grau/2; i++)
-			{
-				free(A11[i]);
-			    free(A12[i]);
-			    free(A21[i]);
-			    free(A22[i]);
-			    free(B11[i]);
-			    free(B12[i]);
-			    free(B21[i]);
-			    free(B22[i]);
-			    free(M1[i]);
-			    free(M2[i]);
-			    free(M3[i]);
-			    free(M4[i]);
-			    free(M5[i]);
-			    free(M6[i]);
-			    free(M7[i]);
-			}	
-			
-		    free(A11);
-		    free(A12);
-		    free(A21);
-		    free(A22);
-		    free(B11);
-		    free(B12);
-		    free(B21);
-		    free(B22);
-		    free(M1);
-		    free(M2);
-		    free(M3);
-		    free(M4);
-		    free(M5);
-		    free(M6);
-		    free(M7);
-		}
-	    
-        return;
-    }
+		for(i = 0; i < grau/2; i++)
+  		{
+	        for(j = 0; j < grau/2; j++)
+	        {
+	            A11[i][j] = 0;
+	            A12[i][j] = 0;
+	            A21[i][j] = 0;
+	            A22[i][j] = 0;
+	            B11[i][j] = 0;
+	            B12[i][j] = 0;
+	            B21[i][j] = 0;
+	            B22[i][j] = 0;
+	            C11[i][j] = 0;
+	            C12[i][j] = 0;
+	            C21[i][j] = 0;
+	            C22[i][j] = 0;
+	            M1[i][j] = 0;
+	            M2[i][j] = 0;
+	            M3[i][j] = 0;
+	            M4[i][j] = 0;
+				M5[i][j] = 0;
+	            M6[i][j] = 0;
+	            M7[i][j] = 0;
+	        }         
+    	}
+	}
 	
 	for(i = 0; i < grau/2; i++)
     {
         for(j = 0; j < grau/2; j++)
         {
             A11[i][j] = A[i]         [j];
-            A12[i][j] = A[i+(grau/2)][j];
-            A21[i][j] = A[i]         [j+(grau/2)];
+            A12[i][j] = A[i]		 [j+(grau/2)];
+            A21[i][j] = A[i+(grau/2)][j];
             A22[i][j] = A[i+(grau/2)][j+(grau/2)];
             B11[i][j] = B[i]         [j];
-            B12[i][j] = B[i+(grau/2)][j];
-            B21[i][j] = B[i]         [j+(grau/2)];
+            B12[i][j] = B[i]		 [j+(grau/2)];
+            B21[i][j] = B[i+(grau/2)][j];
             B22[i][j] = B[i+(grau/2)][j+(grau/2)];
-        }            
-    }
 
+        }         
+    }  
+    
     strassen(soma(A11, A22, grau/2), soma(B11, B22, grau/2), M1, grau/2);
     strassen(soma(A21, A22, grau/2), B11, M2, grau/2);
     strassen(A11, subtrai(A11, A22, grau/2), M3, grau/2);
@@ -171,21 +159,49 @@ void strassen(int **A, int **B, int **C, int grau)
     C11 = subtrai(soma(M1, M4, grau/2), soma(M5, M7, grau/2), grau/2);
     C12 = soma(M3, M5, grau/2);
     C21 = soma(M2, M4, grau/2);
-    C22 = soma(subtrai(M1, M2, grau/2), soma(M3, M6, grau/2), grau/2);
+    C22 = soma(subtrai(M1, M2, grau/2), soma(M3, M6, grau/2), grau/2);  
     
+	printf("C11:\n");
+	for(i = 0; i < grau; i++)
+	{
+		for(j = 0; j < grau; j++)
+			printf("%d   ", C11[i][j]);
+		printf("\n");
+	}
+	
+	printf("C12:\n");
+	for(i = 0; i < grau; i++)
+	{
+		for(j = 0; j < grau; j++)
+			printf("%d   ", C12[i][j]);
+		printf("\n");
+	}
+	
+	printf("C21:\n");
+	for(i = 0; i < grau; i++)
+	{
+		for(j = 0; j < grau; j++)
+			printf("%d   ", C21[i][j]);
+		printf("\n");
+	}
+	
+	printf("C22:\n");
+	for(i = 0; i < grau; i++)
+	{
+		for(j = 0; j < grau; j++)
+			printf("%d   ", C22[i][j]);
+		printf("\n");
+	}
+	
 	for(i = 0; i < grau/2; i++)
-    {
-        for(j = 0; j < grau/2; j++)
-        {
-			C[i][j] = 0;
-			for(j = 0; j < grau/2; j++)
-	        {
-	            C[i]         [j]   		  = C11[i][j];
-	            C[i+(grau/2)][j] 		  = C12[i][j];
-	            C[i]         [j+(grau/2)] = C21[i][j];
-	            C[i+(grau/2)][j+(grau/2)] = C22[i][j];
-	        }     
-		}
+	{
+  		for(j = 0; j < grau/2; j++)
+  	    {
+            C[i]         [j]   		  = C11[i][j];
+            C[i]         [j+(grau/2)] = C12[i][j];
+            C[i+(grau/2)][j] 		  = C21[i][j];
+            C[i+(grau/2)][j+(grau/2)] = C22[i][j];
+        }     
 	}
 
 	{//Libera espaços
@@ -264,14 +280,26 @@ int main(int argc, char** argv)
 	{
 		for(j = 0; j < n; j++)
 		{
-			A[i][j] = rand()%5;
-			B[i][j] = rand()%5;
+			A[i][j] = rand()%2;
+			B[i][j] = rand()%2;
 		}			
 	}
 	
-	printf("%d  %d\n%d  %d\n\n", A[0][0], A[0][1], A[1][0], A[1][1]);
-	//printf("%d  %d\n%d  %d\n\n", B[0][0], B[0][1], B[1][0], B[1][1]);	
-//	printf("%d  %d\n%d  %d\n\n", C[0][0], C[0][1], C[1][0], C[1][1]);
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+			printf("%d   ", A[i][j]);
+		printf("\n");
+	}
+	printf("\n\n\n");
+	
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+			printf("%d   ",B[i][j]);
+		printf("\n");
+	}
+	printf("\n\n\n");
 	
 	strassen(A, B, C, n);
 	
@@ -282,15 +310,16 @@ int main(int argc, char** argv)
 		free(C[i]);
 	}	
 	
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+			printf("%d   ", C[i][j]);
+		printf("\n");
+	}
+	
 	free(A);
 	free(B);
 	free(C);
-
-	printf("%d  %d\n%d  %d\n\n", A[0][0], A[0][1], A[1][0], A[1][1]);
-//	printf("%d  %d\n%d  %d\n\n", B[0][0], B[0][1], B[1][0], B[1][1]);	
-//	printf("%d  %d\n%d  %d\n\n", C[0][0], C[0][1], C[1][0], C[1][1]);
-	
-	printf("FIM");
 	
 	return 0;
 }
